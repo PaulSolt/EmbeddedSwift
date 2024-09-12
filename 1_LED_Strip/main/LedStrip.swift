2a4218e18dbb7384285e1917e835f3885e13da05
Paul Solt <PaulSolt@gmail.com>
2024-09-12 10:01:27 -0400

    Updated sample code

--- 1_LED_Strip/main/LedStrip.swift
+++ 1_LED_Strip/main/LedStrip.swift
@@ -21,21 +21,16 @@ struct LedStrip {
       led_model: LED_MODEL_WS2812,
       flags: .init(invert_out: 0)
     )
-    print("stripConfig: \(stripConfig.max_leds)")
-    // print(stripConfig.max_leds)
+    
     var spiConfig = led_strip_spi_config_t(
       clk_src: SPI_CLK_SRC_DEFAULT,
       spi_bus: SPI2_HOST,
       flags: .init(with_dma: 1)
     )
 
-    print("config: \(spiConfig.clk_src) spi_bus: \(spiConfig.spi_bus)")
     guard led_strip_new_spi_device(&stripConfig, &spiConfig, &handle) == ESP_OK else {
       fatalError("cannot configure spi device")
     }
-
-    
-    print("ledDevice != nil: \(handle != nil)i")
 
     self.handle = handle!
   }
@@ -44,7 +39,11 @@ struct LedStrip {
     var r, g, b: UInt8
     static var white = Color(r: 255, g: 255, b: 255)
     static var lightWhite = Color(r: 6, g: 6, b: 6)
+    static var lightBlue = LedStrip.Color.init(r: 0, g: 0, b: 6)
+
     static var lightRandom: Color { 
+      // FIXME: .random() is crashing on ESP32, not currently using, requires some kind of patch: https://github.com/apple/swift-embedded-examples/issues/17
+      
       Color(r: 1, g: 1, b: 5)
       // Color(r: .random(in: 0...16), g: .random(in: 0...16), b: .random(in: 0...16))
     }
@@ -54,9 +53,6 @@ struct LedStrip {
 
   func setPixel(index: Int, color: Color) {
     led_strip_set_pixel(handle, UInt32(index), UInt32(color.r), UInt32(color.g), UInt32(color.b))
-
-// esp_err_t led_strip_set_pixel_hsv(led_strip_handle_t strip, uint32_t index, uint16_t hue, uint8_t saturation, uint8_t value);
-  // led_strip_set_pixel_hsv(handle, UInt32(index), UInt16(40), UInt8(255), UInt8(color.r))
   }
 
   func setPixel(index: Int, color: Color, hue: UInt16) {
--- 1_LED_Strip/main/Main.swift
+++ 1_LED_Strip/main/Main.swift
@@ -8,115 +8,59 @@
 // See https://swift.org/LICENSE.txt for license information
 //
 //===----------------------------------------------------------------------===//
-// import Foundation
-// func format(double: Double) -> String {
-//     let int = Int(double)
-//     let frac = Int((double - Double(int)) * 10000)
-//     return "\(int).\(frac)"
-// }
 
 @_cdecl("app_main")
 func app_main() {
   print("Hello from Swift on ESP32-C6!")
-  // print(3.14)
-  // print("\(3.14)")
-  // printf("%f", 3.14)
-  printFloat(3.14)
-  // printFloat(Float(3.14))
-  // printFloat(3.14)
-  // char array[10];
-  // sprintf(array, "%f", 3.123);
 
+  // As a debug workaround with Embedded Swift (9/12/24)
+  // You can print floating point numbers in C with printf or using the format(double:) method
+  print("PI= \(format(double: 3.14))")
+  printFloat(3.14)
+  
+  // TODO: Adjust by the number of pixels on your LED light strip or matrix
   let n = 256
   let ledStrip = LedStrip(gpioPin: 0, maxLeds: n)
   ledStrip.clear()
-  print("clear!")
-  var color: LedStrip.Color = .lightRandom
+
+  var color: LedStrip.Color = .lightWhite
   var flip = false
   var colors: [LedStrip.Color] = .init(repeating: .off, count: n)
   var counter = 0
-  var t: Double = 6.4
+  
+  let ledPerRow = 16
+  let rows = 4
 
-  var hue: UInt16 = 40
-
+  // Animation loop
   while true {
-    colors.removeAll()
-    for x in 0..<16 {
-      for y in 0..<16 {
-      
-        let x = Double(x)
-        let y: Double = Double(y)
-        var c = abs(sin(x * y * t)) // * 32
-        print("\(Int(x)), \(Int(y)) \(format(double: c))")
-        // String(format: "%f", 3.0)
-        // print(c)
-        // putc(Int32, UnsafeMutablePointer<FILE>!)
-        // putc('\n', stdout);
 
-        // printf("abc")
-        
-        // let c = abs(sin(x * y * t) * 32 - cos(x * y * t) * 32)
+    // Animate LED rows by removing colors from one side and appending to the other
+    colors.removeLast(ledPerRow)
+    let newColors: [LedStrip.Color] = .init(repeating: color, count: ledPerRow)
+    colors.insert(contentsOf: newColors, at: 0)
 
-        // var c = abs(sin(x * y * t + x * x - y * y)) // - cos(x * y * t)) // 255.0
-        // var c = abs(sin(x * t))
-        // if c > 1 {
-        //     c = 1
-        // }
-        c *= 32
-
-
-
-        //let c = abs( Int(sin(Double(x * y)) * Double(t) + sin(Double(y) + t) ) * 64) % 64
-        // print("x: \(x) y \(y) c: \(c)")
-        // let y = String()
-        // print("\(Float(c))")
-        let color: LedStrip.Color = LedStrip.Color(r: UInt8(c), g: UInt8(c), b: UInt8(c))
-        colors.append(color)
-        // let color = LedStrip.Color(r: UInt8, g: UInt8, b: UInt8)
-      } 
-    }
-
-    // t += 0.001
-   if flip {
-      color = .lightRandom
-    } else {
-      color = .lightWhite
-    }
-
-    // print("colors")
+    // Update all the colors
     for y: Int in 0 ..< 16 {
-
       for x in 0 ..< 16 {
         let index = y * 16 + x
-        // ledStrip.setPixel(index: index, color: color)
-        // ledStrip.setPixel(index: index, color: colors[index])
-        ledStrip.setPixel(index: index, color: colors[index], hue: hue)
+        ledStrip.setPixel(index: index, color: colors[index])
       }
     }
-    print("hue: \(hue)")
-    hue += 1
-    hue %= 360
-
-    // for index in 0 ..< n / 2 {
-    //   print("setPixel: \(n)")
-    //   ledStrip.setPixel(index: index, color: colors[index])
-
-    //   // ledStrip.setPixel(index: index * 2, color: colors[index * 2])
-    //   // ledStrip.setPixel(index: index + 2, color: colors[index + 2])
-    // }
-    // print("refresh:")
     ledStrip.refresh()
 
-    counter += 1
-    if counter >= 6 {
+    // Alternate color every X rows
+    if counter > rows {
       flip.toggle()
       counter = 0
     }
+    if flip {
+      color = .lightBlue
+    } else {
+      color = .lightWhite
+    }
+    counter += 1
 
-    // flip.toggle()
-
-    let blinkDelayMs: UInt32 = 16
+    let blinkDelayMs: UInt32 = 33
     vTaskDelay(blinkDelayMs / (1000 / UInt32(configTICK_RATE_HZ)))
-
   }
 }
